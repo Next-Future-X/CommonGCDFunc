7ebc49f50d2ac7b8a541af38c77a55057c955279
yuanxiangyang <yuanxiangyang@qding.me>
2018-04-08 19:15:05 +0800

    添加工程文件

---
+++ CommonGCDFunc/CommonGCDFunc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CommonGCDFunc
+//
+//  Created by Yuan on 2018/4/8.
+//  Copyright © 2018年 Yuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CommonGCDFunc/CommonGCDFunc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CommonGCDFunc
+//
+//  Created by Yuan on 2018/4/8.
+//  Copyright © 2018年 Yuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
